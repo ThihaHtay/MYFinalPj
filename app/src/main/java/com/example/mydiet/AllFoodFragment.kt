6ea68fe5992141184fa8e5851f41037578c87f37
unknown <thihahtay14@gmail.com>
2020-02-17 11:23:38 +0630

    second

--- app/src/main/java/com/example/mydiet/AllFoodFragment.kt
+++ app/src/main/java/com/example/mydiet/AllFoodFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +19,14 @@ class AllFoodFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_food, container, false)
+        val root: View = inflater.inflate(R.layout.fragment_all_food, container, false)
+        var btnjuice =root.findViewById<Button>(R.id.btn_juice)
+
+        btnjuice.setOnClickListener{view:View->
+            view.findNavController().navigate(R.id.action_allFoodFragment_to_juiceFragment)
+        }
+
+        return root
     }
 
 
